e9c228aa4ee0e835f1ba5f8b80c5e21406c5d9ce
Paul Zupan <paul.zupan.13@gmail.com>
2025-07-23 12:13:51 -0700

    Add glad (build fails)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,61 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
 
+#include "glad/glad.h"
+
+static const std::array<GLfloat, 6> s_vertices = {
+  -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f
+};
+
+static const std::string s_vertex_source = R"glsl(#version 450 core
+in vec2 position;
+void main() {
+  gl_Position = vec4(position, 0.0f, 1.0f);
+}
+)glsl";
+
+static const std::string s_fragment_source R"glsl(#version 450 core
+out vec4 f_color;
+void main() {
+  f_color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+}
+)glsl";
+
 int main() {
   sf::ContextSettings settings;
-  settings.majorVersion = 3;
-  settings.minorVersion = 3;
+  settings.majorVersion = 4;
+  settings.minorVersion = 5;
 
-  sf::Window window{sf::VideoMode{{800, 600}}, "Flora", sf::Style::Default,
-                    sf::State::Windowed, settings};
+  sf::Window window{
+    sf::VideoMode{{800, 600}},
+    "Flora",
+    sf::Style::Default,
+    sf::State::Windowed,
+    settings,
+  };
 
   window.setFramerateLimit(60);
+
+  // Set up vertex data
+  GLuint va;
+  glGenVertexArrays(1, &va);
+  glBindVertexArray(va);
+
+  GLuint vb;
+  glGenBuffers(1, &vb);
+  glBindBuffer(GL_ARRAY_BUFFER, vb);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    s_vertices.size() * sizeof(decltype(s_vertices)::value_type),
+    s_vertices.data(),
+    GL_STATIC_DRAW
+  );
+
+  // Set up buffer layout
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+  glEnableVertexAttribArray(0);
+
+  // Set up shaders
 
   bool window_is_open = true;
   while (window_is_open) {
